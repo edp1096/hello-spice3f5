f8042f173afbbaf5619e295ab181a98fd2db2969
edp1096 <edp1096@naver.com>
2021-01-14 18:15:57 +0900

    *.lnk to *.link

--- spice3f5/src/bin/msc51.bat
+++ spice3f5/src/bin/msc51.bat
@@ -5,22 +5,22 @@ cl /I..\include\ /I..\lib\dev /c tunepc.c >> ..\msc.out
 del cspice.obj
 cl /I..\include\ /c /DSIMULATOR /DSPICE2 /DBATCH main.c >> ..\msc.out
 rename main.obj cspice.obj
-link @cspice.lnk >> ..\msc.out
+link @cspice.link >> ..\msc.out
 del bspice.obj
 cl /I..\include\ /c /DSIMULATOR /DBATCH main.c >> ..\msc.out
 rename main.obj bspice.obj
-link @bspice.lnk >> ..\msc.out
+link @bspice.link >> ..\msc.out
 del nutmeg.obj
 cl /I..\include\ /c main.c >> ..\msc.out
 rename main.obj nutmeg.obj
-link @nutmeg.lnk >> ..\msc.out
+link @nutmeg.link >> ..\msc.out
 cl /I..\include\ /c sconvert.c >> ..\msc.out
-link @sconvert.lnk >> ..\msc.out
+link @sconvert.link >> ..\msc.out
 cl /I..\include\ /c proc2mod.c >> ..\msc.out
-link @proc2mod.lnk >> ..\msc.out
+link @proc2mod.link >> ..\msc.out
 cl /I..\include\ /c help.c >> ..\msc.out
-link @help.lnk >> ..\msc.out
+link @help.link >> ..\msc.out
 cl /I..\include\ /c makeidx.c >> ..\msc.out
-link @makeidx.lnk >> ..\msc.out
+link @makeidx.link >> ..\msc.out
 cl /I..\include\ /c multidec.c >> ..\msc.out
-link @multidec.lnk >> ..\msc.out
+link @multidec.link >> ..\msc.out
